622d21658d575c2e4c6582fbdb3ebea2b356c20e
Phu Tran <phu.th@softworld.vn>
2022-03-04 14:32:02 +0700

    fix

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,82 +1,82 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
-  entry: ['./src/index.js'],
+  entry: ["./src/index.js"],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    chunkFilename: '[name].[chunkhash].js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    chunkFilename: "[name].[chunkhash].js",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      appMountId: 'app',
-      filename: 'index.html',
+      appMountId: "app",
+      filename: "index.html",
       hash: true,
-      template: './index.html',
-    })
+      template: "./public/index.html",
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
+        use: "babel-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.svg$/,
-        use: 'file-loader'
+        use: "file-loader",
       },
       {
         test: /\.png$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
-              mimetype: 'image/png'
-            }
-          }
-        ]
+              mimetype: "image/png",
+            },
+          },
+        ],
       },
       {
         test: /\.(sc|c|sa)ss$/,
         use: [
           {
-            loader: 'style-loader'
+            loader: "style-loader",
           },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               sourceMap: true,
-              importLoaders: 2
-            }
+              importLoaders: 2,
+            },
           },
           // You have to put it after `css-loader` and before any `pre-precessing loader`
-          { loader: 'scoped-css-loader' },
+          { loader: "scoped-css-loader" },
           {
-            loader: 'sass-loader'
-          }
-        ]
+            loader: "sass-loader",
+          },
+        ],
       },
       {
         test: /\.(woff|woff2|eot|ttf)$/,
-        loader: 'url-loader?limit=100000'
-      }
-    ]
+        loader: "url-loader?limit=100000",
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
     alias: {
-      'react-dom': '@hot-loader/react-dom',
-      '@App': path.resolve(__dirname, 'src/')
-    }
+      "react-dom": "@hot-loader/react-dom",
+      "@App": path.resolve(__dirname, "src/"),
+    },
   },
   devServer: {
-    contentBase: './dist',
-    host: 'localhost',
+    contentBase: "./dist",
+    host: "localhost",
     historyApiFallback: true,
-    open: true
-  }
+    open: true,
+  },
 };
 
 module.exports = config;
