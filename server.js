5c36d9e37478785cf52856d7f168636bf53964aa
Phu Tran <phu.th@softworld.vn>
2022-03-04 13:16:28 +0700

    create chat

--- server.js
+++ server.js
@@ -1,31 +1,42 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.use(cookieParser())
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
-app.get('/', (req ,res) => {
-    res.json({msg: "Hello"})
-})
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello" });
+});
 
-const URI = process.env.MONGODB_URL
-mongoose.connect(URI, {
-    useCreateIndex:true,
+const URI = process.env.MONGODB_URL;
+mongoose.connect(
+  URI,
+  {
+    useCreateIndex: true,
     useFindAndModify: false,
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-} , err => {
-    if(err) throw err;
-    console.log("Connected to mongosedb")
-})
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Connected to mongosedb");
+  }
+);
 
-const port = process.env.PORT || 5000
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log('Server is runningon port' ,port)
-})
+  console.log("Server is runningon port", port);
+});
