f943a2e3036269de4f241498d8253be4de59ae6d
Tran Huu Phu <phu.tran@digitalunicorn.fr>
2022-03-13 09:13:51 +0700

    fix deploy

--- server/index.ts
+++ server/index.ts
@@ -71,10 +71,10 @@ mongoose.connect(
 );
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/dist"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
-  });
+  app.use(express.static('client/dist'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'))
+  })
 }
 
 // server listenning
